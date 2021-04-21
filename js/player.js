065d30fcfcc6e4b2fd4a6d778bd674b98e1654c7
HallowedDust5 <charizardexlvl100@gmail.com>
2021-04-21 12:13:12 -0400

    re added reflection over colliding axis

--- js/player.js
+++ js/player.js
@@ -44,13 +44,15 @@ class Player{
             for(let i=0; i<arr.length;i++){
                 let collideTest= new SAT.Response();
                 if(SAT.testPolygonPolygon(this.pVector.toPolygon(),arr[i].pVector.toPolygon(),collideTest)){
+                    this.vVector.reverse();
                     if(collideTest.overlapN.y!=0){
                         this.pVector.pos.y+=-1*collideTest.overlapV.y;
+                        this.vVector.x*=-1;
                     }
                     if(collideTest.overlapN.x!=0){
                         this.pVector.pos.x+=-1*collideTest.overlapV.x;
+                        this.vVector.y*=-1;
                     }
-                    this.vVector.reverse();
                     
                     
                     
