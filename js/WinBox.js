d22adf9d219fcb93503d9c37b4b2e42617aad1a9
HallowedDust5 <charizardexlvl100@gmail.com>
2021-04-22 11:42:34 -0400

    WinBox implemented

---
+++ js/WinBox.js
@@ -0,0 +1,7 @@
+class WinBox extends Box{
+    constructor(tempWidth,tempHeight, tempx,tempy,tempxspeed=0,tempyspeed=0,tempwspeed=0,temphspeed=0){
+        super(tempWidth,tempHeight, tempx,tempy,tempxspeed,tempyspeed,tempwspeed,temphspeed);
+    }
+
+
+}
